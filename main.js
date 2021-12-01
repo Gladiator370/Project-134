1d3c5aae61b87a0a5b2891b6f56b8373eb083ea8
Gladiator370 <75198510+Gladiator370@users.noreply.github.com>
2021-12-01 13:11:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function bedroom(){
+    window.location = "bedroom.html";
+}
+function living(){
+    window.location = "living.html";
+}
+function balcony(){
+    window.location = "balcony.html";
+}
+function kitchen(){
+    window.location = "kitchen.html";
+}
+function myroom(){
+    window.location = "room.html";
+}
